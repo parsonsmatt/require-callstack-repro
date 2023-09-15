4cda84cccbb1ebea3f4de06785a989ef6b88a829
parsonsmatt <parsonsmatt@gmail.com>
2023-09-15 14:38:57 -0600

    ok

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,20 +1,46 @@
-{-# language RankNTypes, MultiParamTypeClasses, DataKinds, ConstraintKinds, ImplicitParams, UndecidableInstances #-}
+{-# language RankNTypes, TypeFamilies, KindSignatures, MultiParamTypeClasses, DataKinds, ConstraintKinds, ImplicitParams, UndecidableInstances, FlexibleContexts, FlexibleInstances #-}
 
 {-# OPTIONS_GHC -Wno-orphans -Wno-missing-methods #-}
 
 module Lib where
 
+import Data.Kind
 import GHC.Stack (HasCallStack)
 import GHC.Classes (IP(..))
 import GHC.TypeLits (TypeError, ErrorMessage(..))
 
-foo :: RequireCallStack => IO ()
-foo = do
+-- The Problem:
+--
+-- GHC complains about `bar` with the `TypeError` in the `instance IP
+-- "provideCallStack" ProvideCallStack`.
+--
+-- Removing the type error allows the program to compile without error.
+--
+-- Adding a type signature to `bar` that specifies `RequireCallStack` also
+-- fixes the issue.
+someFunc :: RequireCallStack => IO ()
+someFunc = do
     errorRequireCallStack "asdf"
 
     let bar = errorRequireCallStack "qwer"
 
     bar
+
+alsoWeird :: IO ()
+alsoWeird = provideCallStack $ do
+    -- `RequireCallStack` should be a satisfied constraint here, as
+    -- evidenced by this building:
+    someFunc
+
+    -- But we get an error in this let binding.
+    let bar = errorRequireCallStack "qwer"
+
+    bar
+
+errorRequireCallStack :: RequireCallStack => String -> x
+errorRequireCallStack = error
+
+instance TypeError ('Text "Add RequireCallStack to your function context or use provideCallStack") => IP "provideCallStack" ProvideCallStack
 
 type RequireCallStack = (HasCallStack, RequireCallStackImpl)
 
@@ -22,13 +48,7 @@ type RequireCallStackImpl = ?provideCallStack :: ProvideCallStack
 
 data ProvideCallStack = ProvideCallStack
 
-errorRequireCallStack :: RequireCallStack => String -> x
-errorRequireCallStack = error
-
--- instance TypeError ('Text "Add RequireCallStack to your function context or use provideCallStack") => IP "provideCallStack" ProvideCallStack
-
 provideCallStack :: (RequireCallStackImpl => r) -> r
 provideCallStack r = r
   where
     ?provideCallStack = ProvideCallStack
-
